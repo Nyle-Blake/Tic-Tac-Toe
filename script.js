acc2ddbef4b57f6f6a9b44e424f906cae1df7df5
Nyle-Blake <Nyleblake1@gmail.com>
2024-02-28 22:43:27 +0000

    Make patterns that player chooses recognised to be a win
    Use array.some on winningombinations array to see if any combination of
    divs marked corresponds to a win, along with array.every to check each
    number in the a combination is in the playerX array
    
    Only one player can play at this time and after winning you can still
    mark more divs

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-const winningombinations = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9],
-  [1,4,7],
-  [2,5,8],
-  [3,6,9],
-  [1,5,9],
-  [3,5,7]
+const winningCombinations = [
+  ["1","2","3"],
+  ["4","5","6"],
+  ["7","8","9"],
+  ["1","4","7"],
+  ["2","5","8"],
+  ["3","6","9"],
+  ["1","5","9"],
+  ["3","5","7"]
 ]
+const playGame = () => {
 
-let playerX = []
-let playerO = []
+  let playerX = []
+  let playerO = []
 
-const gridItem = document.querySelectorAll(".grid-item")
+  const result = document.querySelector(".result")
+  const gridItems = document.querySelectorAll(".grid-item")
 
-gridItem.forEach(item => {
-  item.addEventListener("click", () => {
-    if (item.textContent == "") {
-    item.textContent = "x"
-    playerX.push(item.id)
-    console.log(playerX)
-    }
+  let tester = winningCombinations.some(combination => {
+    return combination.every(elem => playerX.includes(elem))
   })
-});
+
+  gridItems.forEach(item => {
+    item.addEventListener("click", () => {
+      if (item.textContent == "") {
+        item.textContent = "x"
+        playerX.push(item.id)
+        console.log(playerX)
+      }
+
+      let matches = winningCombinations.some(combination => {
+        return combination.every(elem => playerX.includes(elem))
+      })
+
+      if (matches == true) {
+        result.textContent = "You win!"
+      }
+
+      console.log(matches)
+    })
+  });
+
+
+}
+
+playGame()
