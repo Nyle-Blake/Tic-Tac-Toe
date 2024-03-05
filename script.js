0dabbca187a1d5e3882f6a0c2fa4ca64c799e9aa
Nyle-Blake <Nyleblake1@gmail.com>
2024-03-05 01:30:23 +0000

    add buttons for player to choose symbol and reset the grid and counts

--- script.js
+++ script.js
@@ -11,41 +11,78 @@ const winningCombinations = [
 
 const result = document.querySelector(".result")
 const gridItems = document.querySelectorAll(".grid-item")
+const symbolBtnX = document.querySelector(".choiceX")
+const symbolBtnO = document.querySelector(".choiceO")
 
-let playerSymbol = "x"
 
-let playerX = []
-let playerO = []
+let playerSymbol = ""
 
-// function for player to select square they want marked
+let playerXCount = []
+let playerOCount = []
+
+// function for the player to pick a square to mark
 const getPlayerChoice = (item) => {
 
   if (item.textContent == "") {
-    item.textContent = "x"
-    playerX.push(item.id)
-    console.log(playerX)
+    if (playerSymbol == "x") {
+      item.textContent = "x"
+      playerXCount.push(item.id)
+      console.log(playerXCount)
+    } else if (playerSymbol == "o") {
+      item.textContent = "o"
+      playerOCount.push(item.id)
+      console.log(playerOCount)
+    }
   }
 
 }
 
-// function to get the computer to pick a square to mark
+// function for the computer to pick a square to mark
 const getComputerChoice = () => {
   let computerChoice = ["1", "2", "3", "4", "5", "6", "7", "8", "9",][Math.floor(Math.random() * 9)]
 
   let chosenSquare = document.getElementById(computerChoice)
 
   if (chosenSquare.textContent == "") {
-    chosenSquare.textContent = "o"
-    playerO.push(computerChoice)
+    if (playerSymbol == "x") {
+      chosenSquare.textContent = "o"
+      playerOCount.push(computerChoice)
+    } else if (playerSymbol == "o") {
+      chosenSquare.textContent = "x"
+      playerXCount.push(computerChoice)
+    }
   } else {
     getComputerChoice()
   }
 
 }
 
-// grid square event listener
+// click event listener to allow player to choose x and reset the grid
+symbolBtnX.addEventListener("click", () => {
+  playerSymbol = "x"
+  gridItems.forEach(item => {
+    item.textContent = ""
+  })
+  playerXCount = []
+  result.textContent = ""
+  console.log(playerXCount)
+})
+
+// click event listener to allow player to choose o and reset the grid
+symbolBtnO.addEventListener("click", () => {
+  playerSymbol = "o"
+  gridItems.forEach(item => {
+    item.textContent = ""
+  })
+  playerOCount = []
+  result.textContent = ""
+  console.log(playerOCount)
+})
+
+// grid square click event listener
 gridItems.forEach(item => {
   item.addEventListener("click", () => {
+    console.log(playerSymbol)
     getPlayerChoice(item)
     getComputerChoice()
     checkForWin()
@@ -55,12 +92,18 @@ gridItems.forEach(item => {
 // function to check wether a winning combination has been made
 const checkForWin = () => {
 
-  let matches = winningCombinations.some(combination => {
-    return combination.every(elem => playerX.includes(elem))
+  let matchesX = winningCombinations.some(combination => {
+    return combination.every(elem => playerXCount.includes(elem))
   })
 
-  if (matches == true) {
-    result.textContent = "You win!"
+  let matchesO = winningCombinations.some(combination => {
+    return combination.every(elem => playerOCount.includes(elem))
+  })
+
+  if (matchesX == true) {
+    result.textContent = "X wins!"
+  } else if (matchesO == true) {
+    result.textContent = "O wins";
   }
 
 }
